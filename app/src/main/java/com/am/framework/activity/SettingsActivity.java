fd20dd1e66b9ab2f68ff9d9caa4cc9378ce64855
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-08-21 16:57:44 +0300

    Add Settings Activity

---
+++ app/src/main/java/com/am/framework/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.am.framework.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.am.framework.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
