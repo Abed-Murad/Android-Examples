28a7914333cc9653f5a6f29af2a5490a62672959
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-08-21 17:06:55 +0300

    Add two CheckBoxPref to pref_settings.xml

---
+++ app/src/main/java/com/am/framework/fragment/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.am.framework.fragment;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener , Preference.OnPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
