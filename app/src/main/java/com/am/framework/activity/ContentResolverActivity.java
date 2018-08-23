f722dfa05e916aeeda0ddf0a839c1058f8461234
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-08-24 00:09:05 +0300

    Add sohwDefinition() & fetchNextWord()

--- app/src/main/java/com/am/framework/activity/ContentResolverActivity.java
+++ app/src/main/java/com/am/framework/activity/ContentResolverActivity.java
@@ -1,5 +1,7 @@
 package com.am.framework.activity;
 
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -24,16 +26,50 @@ public class ContentResolverActivity extends AppCompatActivity {
     TextView textViewDefinition;
 
 
+    private Cursor mData;
+    private int mCrrentState;
+    private int mDefinitionColumn;
+    private int mWordColumn;
+    private final int STATE_HIDDEN = 0;
+    private final int STATE_SHOWN = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_resolver);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-
     }
 
     public void onButtonClick(View view) {
+        switch (mCrrentState) {
+            case STATE_HIDDEN:
+                showDefinition();
+                break;
+            case STATE_SHOWN:
+                fetchNextWord();
+        }
     }
+
+    private void showDefinition() {
+    }
+
+    private void fetchNextWord() {
+    }
+
+    private class WordFetchTask extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+
+            super.onPostExecute(cursor);
+        }
+    }
+
+
 }
