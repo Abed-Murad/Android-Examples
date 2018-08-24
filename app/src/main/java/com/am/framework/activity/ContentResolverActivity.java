d4f5e1c5106a2f331da73fd3d968940cbe37fa78
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-08-24 14:55:30 +0300

    Refactor ContentResolver Activity

--- app/src/main/java/com/am/framework/activity/ContentResolverActivity.java
+++ app/src/main/java/com/am/framework/activity/ContentResolverActivity.java
@@ -1,5 +1,6 @@
 package com.am.framework.activity;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.AsyncTask;
@@ -15,9 +16,10 @@ import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-
-
 public class ContentResolverActivity extends BaseActivity {
+
+    private final int VISIBILITY_HIDDEN = 0;
+    private final int VISIBILITY_SHOWN = 1;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -28,14 +30,12 @@ public class ContentResolverActivity extends BaseActivity {
     @BindView(R.id.text_view_definition)
     TextView mDefinitionTextView;
 
+    private Cursor mFetchedTableData;
 
-    private Cursor mData;
-    private int mCrrentState;
-    private int mDefinitionColumn;
-    private int mWordColumn;
-    private final int STATE_HIDDEN = 0;
-    private final int STATE_SHOWN = 1;
-
+    //Used To Know if the Definition TextView is Visible or InVisible
+    private int mDefinitionTextViewVisibilityCurrentState;
+    //Used to Retrieve Data From Cursor
+    private int mDefinitionColumnIndex, mWordColumnIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,68 +45,78 @@ public class ContentResolverActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         showToolbarBackArrow();
 
-        new WordFetchTask().execute();
+        //Retrieve All The Words From The Content Provider Using The Content Resolver
+        new WordsFetchingTask().execute();
     }
 
-    public void onButtonClick(View view) {
-        switch (mCrrentState) {
-            case STATE_HIDDEN:
+    public void onClickNextWordBtn(View view) {
+        switch (mDefinitionTextViewVisibilityCurrentState) {
+            case VISIBILITY_HIDDEN:
                 showDefinition();
                 break;
-            case STATE_SHOWN:
+            case VISIBILITY_SHOWN:
                 fetchNextWord();
         }
     }
 
-
-
+    /**
+     * Used to Fetch The Next word form Cursor then show the word on the Screen and Hide the
+     * Definition tell the user click on nextWordBtn
+     */
     private void fetchNextWord() {
-        if (mData != null) {
+
+        if (mFetchedTableData != null) {
             // Move to the next position in the cursor, if there isn't one, move to the first
-            if (!mData.moveToNext()) {
-                mData.moveToFirst();
+            if (!mFetchedTableData.moveToNext()) {
+                mFetchedTableData.moveToFirst();
             }
             mDefinitionTextView.setVisibility(View.INVISIBLE);
             nextBtn.setText(getString(R.string.text_btn_show_definition));
 
-            mWordTextView.setText(mData.getString(mWordColumn));
-            mDefinitionTextView.setText(mData.getString(mDefinitionColumn));
+            String word = mFetchedTableData.getString(mWordColumnIndex);
+            String wordDefinition = mFetchedTableData.getString(mDefinitionColumnIndex);
 
-            mCrrentState = STATE_HIDDEN;
+            mWordTextView.setText(word);
+            mDefinitionTextView.setText(wordDefinition);
+
+            mDefinitionTextViewVisibilityCurrentState = VISIBILITY_HIDDEN;
         }
     }
 
     private void showDefinition() {
-        if (mData != null) {
+        if (mFetchedTableData != null) {
             mDefinitionTextView.setVisibility(View.VISIBLE);
             nextBtn.setText(getString(R.string.text_btn_fetch_next_word));
-            mCrrentState = STATE_SHOWN;
+            mDefinitionTextViewVisibilityCurrentState = VISIBILITY_SHOWN;
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class WordsFetchingTask extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            // Fetch The Data From the Content Provider
+            ContentResolver contentResolver = getContentResolver();
+            return contentResolver.query(DroidTermsExampleContract.CONTENT_URI,
+                    null, null, null, null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+
+            mFetchedTableData = cursor;
+
+            //get the Columns Indexes [Word Column , Definition Column]
+            mDefinitionColumnIndex = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+            mWordColumnIndex = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mData = null;
+        mFetchedTableData = null;
     }
-
-    private class WordFetchTask extends AsyncTask<Void, Void, Cursor> {
-        @Override
-        protected Cursor doInBackground(Void... voids) {
-            ContentResolver contentResolver = getContentResolver();
-            Cursor cursor = contentResolver.query(DroidTermsExampleContract.CONTENT_URI,
-                    null, null, null, null);
-            return cursor;
-        }
-
-        @Override
-        protected void onPostExecute(Cursor cursor) {
-            mData = cursor;
-            mDefinitionColumn = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
-            mWordColumn = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
-            super.onPostExecute(cursor);
-        }
-    }
-
 
 }
