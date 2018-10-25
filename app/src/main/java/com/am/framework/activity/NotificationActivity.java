06a62b7ba72432f9c6f7d9b030ed243bbd0c3075
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-10-25 22:01:57 +0200

    Create NotificationActivity

---
+++ app/src/main/java/com/am/framework/activity/NotificationActivity.java
@@ -0,0 +1,15 @@
+package com.am.framework.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.am.framework.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
