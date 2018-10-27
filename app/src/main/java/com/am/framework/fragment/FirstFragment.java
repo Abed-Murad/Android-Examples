73ecee808423abc03d62e6ed1d86fa5bc72b4f4c
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-10-27 16:34:17 +0200

    Refactor FirstFragment

--- app/src/main/java/com/am/framework/fragment/FirstFragment.java
+++ app/src/main/java/com/am/framework/fragment/FirstFragment.java
@@ -21,12 +21,12 @@ import com.am.framework.R;
 public class FirstFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_FIRST_PARAM = "first_param";
+    private static final String ARG_SECOND_PARAM = "second_param";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mFirstParam;
+    private String mSecondParam;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,16 +38,16 @@ public class FirstFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param firstParam Parameter 1.
+     * @param secondParam Parameter 2.
      * @return A new instance of fragment FirstFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
+    public static FirstFragment newInstance(String firstParam, String secondParam) {
         FirstFragment fragment = new FirstFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_FIRST_PARAM, firstParam);
+        args.putString(ARG_SECOND_PARAM, secondParam);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +56,8 @@ public class FirstFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mFirstParam = getArguments().getString(ARG_FIRST_PARAM);
+            mSecondParam = getArguments().getString(ARG_SECOND_PARAM);
         }
     }
 
@@ -92,16 +92,6 @@ public class FirstFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
