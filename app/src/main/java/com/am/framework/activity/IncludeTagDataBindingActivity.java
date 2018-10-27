4c179f11702748ca6b10f030f2636852dfd72a74
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-10-28 00:33:23 +0200

    Add ClickHandlers For DataBinding

--- app/src/main/java/com/am/framework/activity/IncludeTagDataBindingActivity.java
+++ app/src/main/java/com/am/framework/activity/IncludeTagDataBindingActivity.java
@@ -1,22 +1,29 @@
 package com.am.framework.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.am.framework.R;
+import com.am.framework.databinding.ActivityIncludeTagDataBindingBinding;
+import com.am.framework.model.Item;
 
 public class IncludeTagDataBindingActivity extends AppCompatActivity {
+    ActivityIncludeTagDataBindingBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_include_tag_data_binding);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_include_tag_data_binding);
+        Item item = new Item(101, "<include> Binding", "Binding Message");
+        mBinding.setItem(item);
+        setSupportActionBar(mBinding.toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +33,21 @@ public class IncludeTagDataBindingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public class MyClickHandlers {
+        public void onFabClicked(View view, Item item) {
+            Toast.makeText(getApplicationContext(), "FAB Clicked ! , " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+
+        // You can also pass params while binding.
+        // public void onButtonClickWithParam(View view, User user)
+        // receives the user object bind from UI layout. In the layout,
+        // the parameter can be passed using
+        public boolean onFabLongPressed(View view) {
+            Toast.makeText(getApplicationContext(), "FAB Long Clicked !", Toast.LENGTH_SHORT).show();
+            return true;
+        }
     }
 
 }
