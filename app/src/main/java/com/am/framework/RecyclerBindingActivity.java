46f489c857737d69c2f44fffbd73fd1ba360f096
Abdallah.Murad <Abdallah.Murad@mail.com>
2018-10-28 02:46:28 +0200

    Add Some stuff

---
+++ app/src/main/java/com/am/framework/RecyclerBindingActivity.java
@@ -0,0 +1,13 @@
+package com.am.framework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclerBindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_binding);
+    }
+}
